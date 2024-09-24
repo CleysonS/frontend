25ed91a3112b002be43b6dd7c05da588b2b33fd0
Cleyson Santos <85909144+CleysonS@users.noreply.github.com>
2024-09-24 19:24:16 -0300

    Update Addition.java

--- Addition.java
+++ Addition.java
@@ -1,41 +1,30 @@
-// Fig. 12.2: Addition.java
-// Addition program that uses JOptionPane for input and output.
-import javax.swing.JOptionPane; 
+// Na linha abaixo está importando a biblioteca (pacote) javax, o modulo swing e o componente JOptionPane
 
+import javax.swing.JOptionPane; 
+// Na linha 
 public class Addition 
 {
+   // Na linha abaixo esta executando metodo main 
    public static void main(String[] args)
    {
-      // obtain user input from JOptionPane input dialogs
+      // Nessa linhas esta sendo declarados a varialvel firsNumber o tipo string, aonde tem operador de atribuição tem  indicador de variaveis.
+      // JOptionPane estou invocando ele dentro dele existe o metodo showInputDialog invocado uma caixa de dialogo. 
+    
       String firstNumber = 
-         JOptionPane.showInputDialog("Enter first integer");
+         JOptionPane.showInputDialog("Digite o primeiro numero inteiro: ");
       String secondNumber =
-         JOptionPane.showInputDialog("Enter second integer");
+         JOptionPane.showInputDialog("Digite o segundo numero inteiro: ");
 
-      // convert String inputs to int values for use in a calculation
+      // Vai converter o texto de String em valores interios para uso em um cálculo
       int number1 = Integer.parseInt(firstNumber); 
       int number2 = Integer.parseInt(secondNumber);
-
+       // somatorio de dois variaveis numero 1 e numero 2.
       int sum = number1 + number2; // add numbers
 
-      // display result in a JOptionPane message dialog
-      JOptionPane.showMessageDialog(null, "The sum is " + sum, 
+      // Vai exibir o resultado em uma caixa de diálogo de mensagem JOptionPane
+      JOptionPane.showMessageDialog(null, "A soma e: " + sum, 
          "Sum of Two Integers", JOptionPane.PLAIN_MESSAGE);
    } 
-} // end class Addition
+} 
 
 
-/**************************************************************************
- * (C) Copyright 1992-2014 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
