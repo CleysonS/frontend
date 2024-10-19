809c0d3260f31601543fe8d6eecb12a2e1abf03c
Cleyson Santos <85909144+CleysonS@users.noreply.github.com>
2024-10-19 10:05:14 -0300

    Add files via upload

--- TelaDeLogin.java
+++ TelaDeLogin.java
@@ -38,53 +38,62 @@ public class TelaDeLogin extends JFrame
      lblNotificacoes = new JLabel("Notificações", SwingConstants.CENTER);
         add(lblNotificacoes);
 
+        ButtonHandler buttonHandler = new ButtonHandler();
+        btnLogar.addActionListener(buttonHandler);
 
+        txtSenha.addKeyListener(
+         new KeyAdapter() {
+             @Override
+             public void keyReleased(KeyEvent e) {
+                 if (String.valueOf(txtSenha.getPassword()).trim().length() > 0) {
+                     if (e.getKeyCode() == 10) {
+                         System.out.println("Você teclou Enter");
+                         logar();
+                     }
+                 }
+             }
+         }
+     );
  }
 
-     private class ButtonHandler implements ActionListener{
-      @Override
-      public void actionPerformed(ActionEvent event ){
-         try{
-            Connection conexao = MySQLConnector.conectar();
-            String strSqlLogin = "select * from `db_senac`.`tbl_senac` where `email` = '" + 
-            txtLogin.getText() + "'and `senha` = '" + String.valueOf(txtSenha.getPassword()) + "';";
-            Statement stmSqllogin = conexao.createStatement();
-            ResultSet rstSqlLogin = stmSqllogin.executeQuery(strSqlLogin);
-            if(rstSqlLogin.next()) {
-               //aqui vamos notificar o usuario que o login e senha foi encontrado
-               lblNotificacoes.setText(("conectado com sucesso!"));
-
-            }else {
-               lblNotificacoes.setText(("Login e/ou senha não encontrado! Por favor, verifique e tente novamente."));
-            }
-
-         }catch (Exception e){
-           
-            System.err.println("Ops! Deu ruim, se liga no erros" + e);
-         } 
-
-      }
-
+ private class ButtonHandler implements ActionListener {
+     @Override
+     public void actionPerformed(ActionEvent event) {
+         logar();
+         // aqui será invocado o metodo de logar
      }
+ }
 
-     public String setHtmlFormat(String txt) {
-      return "<html><body>" + txt + "</body></html>";
-  }
+ public void logar() {
+     try {
+         Connection conexao = MySQLConnector.conectar();
+         String strSqlLogin = "select * from `db_senac`.`tbl_senac` where email = '" + txtLogin.getText() + "' and senha = '" + String.valueOf(txtSenha.getPassword()) + "';";
+         Statement stmSqlLogin = conexao.createStatement();
+         ResultSet rstSqlLogin = stmSqlLogin.executeQuery(strSqlLogin);
+         if (rstSqlLogin.next()) {
+             notificarUsuario("Login realizado com sucesso.");
+             // aqui vamos notificar o usuario notificar encontrado o login
+             notificarUsuario("Não foi possível encontrar o login e/ou senha digitados. Por favor, verifique e tente novamente.");
+             // aqui vamos notificar o usuario que o login e senha não foram encontrados
+         }
+     } catch (Exception e) {
+         notificarUsuario("Houve um problema e não será possível realizar o login neste momento. Por favor, tente novamente mais tarde.");
+         System.err.println("Ops! Deu ruim, se liga no erro: " + e);
+     }
+ }
 
-  public void notificarUsuario(String strTexto) {
-      lblNotificacoes.setText(setHtmlFormat(strTexto));
-  }
- public static void main(String[] args){
-   
-   TelaDeLogin appTelaDeLogin = new TelaDeLogin();
-   appTelaDeLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-   appTelaDeLogin.setSize(200,140);
-   appTelaDeLogin.setVisible(true);
+ public String setHtmlFormat(String txt) {
+     return "<html><body>" + txt + "</body></html>";
+ }
 
-   
-  }
+ public void notificarUsuario(String strTexto) {
+     lblNotificacoes.setText(setHtmlFormat(strTexto));
+ }
 
-
-    
-
+ public static void main(String[] args) {
+     TelaDeLogin appTelaDeLogin = new TelaDeLogin();
+     appTelaDeLogin.setDefaultCloseOperation(EXIT_ON_CLOSE);
+     appTelaDeLogin.setSize(150,200);
+     appTelaDeLogin.setVisible(true);
+ }
 }
